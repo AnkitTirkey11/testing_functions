6a410488afcbdaeae1ec3e9b01d56f8200a7789b
AnkitTirkey11 <bryantirkey11@gmail.com>
2021-06-17 15:09:12 +0530

    First commit

---
+++ test_sum.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+
+def test_add():
+    assert add(1,2) == 3,"test Fail"
+
+def test_subtract():
+    assert subtract(3,2) == 1,"Test Fail"
