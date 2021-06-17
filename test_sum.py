f5cba0b81ad3e3f577495f27759e298ca8160ecf
AnkitTirkey11 <bryantirkey11@gmail.com>
2021-06-17 17:10:07 +0530

    Changed few more functions

--- test_sum.py
+++ test_sum.py
@@ -7,28 +7,34 @@ def multiplication(a,b):
     return a*b
 
 def test_add():
-    assert add(1,2) == 3,"Test Failed"
+    print(3)
+    assert False
+    #assert add(1,2) == 3,"Test Failed"
     #if (add(1,2) == 3):
     #    print(True)
 
 def test_subtract():
-    assert subtract(3,2) == 1,"Test Fail"
+    print(3)
+    #assert subtract(3,2) == 1,"Test Fail"
     #if subtract(3,2) == 1:
     #    print(True)
 
 def test_sub():
-    assert subtract(4,2) == 2,"Test Fail"
+    print(3)
+    #assert subtract(4,2) == 2,"Test Fail"
     #if subtract(4,2) == 2:
     #    print(True)
 
 def test_multiplication():
-    assert multiplication(4,4) == 16,"Test Fail"
+    print(3)
+    #assert multiplication(4,4) == 16,"Test Fail"
     #if multiplication(4,4) == 16:
     #    print(True)
 
 def test_mult():
-    assert multiplication(3,3) == 9,"Test Failed"
-    assert multiplication(3, 5) == 15, "Test Failed"
+    print(3)
+    #assert multiplication(3,3) == 9,"Test Failed"
+    #assert multiplication(3, 5) == 15, "Test Failed"
 #test_add()
 #test_subtract()
 #test_sub()
